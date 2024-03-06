3c7a1569cfc2265ef80d7ff09e1553808ed1dcdf
Maxorella <maxorella@mail.ru>
2024-03-06 10:58:51 +0300

    added some tests

--- internal/handler/login_test.go
+++ internal/handler/login_test.go
@@ -1,31 +1,74 @@
-package handler
+package handler_test
 
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/go-park-mail-ru/2024_1_CyberHedgehogs/internal/repository"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	rep "github.com/go-park-mail-ru/2024_1_CyberHedgehogs/internal/repository"
+	"github.com/go-park-mail-ru/2024_1_CyberHedgehogs/internal/handler"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
+func setupRouter() *mux.Router {
+	userTable := &repository.UserTable{Users: make(map[string]*repository.User)}
+	sessionTable := &repository.SessionTable{Sessions: make(map[string]*repository.Session)}
+
+	testUser := &repository.User{
+		Login:    "testUser",
+		Username: "Test User",
+		Email:    "testuser@example.com",
+		Password: "password123",
+	}
+	userTable.Users[testUser.Login] = testUser
+
+	authHandler := handler.NewAuthHandler(userTable, sessionTable)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/login", authHandler.Login).Methods("POST")
+
+	return r
+}
+
 func TestLogin(t *testing.T) {
-	userTable := &rep.UserTable{Users: make(map[string]*rep.User)}
-	sessionHandler := NewSessionHandler(userTable)
+	tests := []struct {
+		name           string
+		payload        map[string]string
+		expectedStatus int
+	}{
+		{
+			name: "Successful login",
+			payload: map[string]string{
+				"login":    "testUser",
+				"password": "password123",
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name: "Invalid login credentials",
+			payload: map[string]string{
+				"login":    "testUser",
+				"password": "wrongPassword",
+			},
+			expectedStatus: http.StatusUnauthorized,
+		},
+	}
 
-	user := rep.User{Login: "testLogin", Password: "testPass"}
-	userTable.Users[user.Login] = &user
+	r := setupRouter()
 
-	loginData := map[string]string{"login": "testLogin", "password": "testPass"}
-	body, _ := json.Marshal(loginData)
-	req, _ := http.NewRequest("POST", "/login", bytes.NewBuffer(body))
-	w := httptest.NewRecorder()
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			payloadBytes, _ := json.Marshal(tc.payload)
+			req, _ := http.NewRequest("POST", "/login", bytes.NewBuffer(payloadBytes))
+			req.Header.Add("Content-Type", "application/json")
 
-	sessionHandler.Login(w, req)
+			rr := httptest.NewRecorder()
+			r.ServeHTTP(rr, req)
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	cookie := w.Result().Cookies()
-	assert.NotEmpty(t, cookie, "Cookie 'session_id' should not be empty")
+			assert.Equal(t, tc.expectedStatus, rr.Code)
+		})
+	}
 }
